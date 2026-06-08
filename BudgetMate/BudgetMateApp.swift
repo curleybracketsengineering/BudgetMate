fb0f5707fa02ab3ae0f105e6aa1e2ab8fc13f2ca
scottmatheson58 <scott@matheson.it>
2026-06-08 13:11:18 +0100

    Initial Commit

---
+++ BudgetMate/BudgetMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetMateApp.swift
+//  BudgetMate
+//
+//  Created by scott matheson on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BudgetMate
+//
+//  Created by scott matheson on 08/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
