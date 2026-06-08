eabfea38d9aaec1ced7dc760631fd8c33cdade5f
scottmatheson58 <scott@matheson.it>
2026-06-08 13:39:27 +0100

    fist commet

--- BudgetMate/ContentView.swift
+++ BudgetMate/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Budget Mate!")
+            Text("Budget Mate!!")
         }
         .padding()
     }
